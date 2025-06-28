b9b52d9bf67a671d349a4691225af24d807c5560
David Offei Akuffo <david.akuffo11@gmail.com>
2025-06-28 00:18:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const sideMenu = document.querySelector("#sideMenu");
+function openMenu() {
+  sideMenu.style.transorm = "translateX(-16rem)";
+}
+function closeMenu() {
+  sideMenu.style.transorm = "translateX(s16rem)";
+}
