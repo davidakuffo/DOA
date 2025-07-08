9592920270ee0dc1912b774669d07fd0a75e5edf
David Offei Akuffo <david.akuffo11@gmail.com>
2025-07-08 14:03:44 +0000

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    fontFamily: {
+      Outfit: ["Outfit", "sans-serif"],
+      Ovo: ["Ovo", "serif"],
+    },
+    colors: {
+      darkTheme: "#11001F",
+    },
+  },
+  darkMode: "selector",
+};
